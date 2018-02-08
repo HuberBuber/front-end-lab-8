9faa6e8f9b769b2518130a4d5fec4fb3e12e6857
Oleksandr <oleksandr1991xperia@gmail.com>
2018-02-08 23:29:48 +0200

    home-7

---
+++ homework_07/js/task1.js
@@ -0,0 +1,26 @@
+
+var n, i, j, space, brick;
+
+	n = +prompt("Please enter a number from 1 to 20", "0");
+
+if (n <=0 || n > 20 ) {
+	console.log('Incorrect');
+} else {
+		for (i = 0; i < n; i++){
+			space = '';
+			brick = '';
+			for (j = 0; j < n - i; j++) {
+				space = space + '   ';
+			}
+			for (j = 0; j < 2 * i + 1; j++){
+				brick = brick + '[~]';
+			}
+			console.log(space + brick);
+		}
+	}
+
+
+
+
+
+
