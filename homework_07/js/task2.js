18ae9af7623a143cc72360ac0c8fdaeda1130bc4
Oleksandr <oleksandr1991xperia@gmail.com>
2018-02-11 14:12:54 +0200

    home-7 new changes in task2

--- homework_07/js/task2.js
+++ homework_07/js/task2.js
@@ -12,30 +12,25 @@ var goOn = true;
 	if (game == true){
 
 		while (goOn == true){
+
 			prizeNumber = Math.floor(Math.random() * (max - min + 1));
 			possiblePrize = maxPrize;
-				for (var i = 3; i > 0; i--) {
-					
-		     	//console.log(prizeNumber);
 
+				for (var i = 3; i > 0; i--) {
 					userNumber = +prompt("Enter a number from 0 to " + max + "\n" + "Attemts left: " + i +  "\n" + "Total prize: " + totalPrize + "$\n" + "Possible prize in current attempt: " + possiblePrize + "$");
-					if (userNumber < 0 || isNaN(userNumber)){
+					if (userNumber < 0 || isNaN(userNumber) || userNumber > max){
 						console.error("Error, wrong number!");
-					}
-						
-					if (i == 3 && prizeNumber == userNumber){
+					} else if (i == 3 && prizeNumber == userNumber){
 						prize = maxPrize;
-						//console.log('prize is ' + prize + '$');
 						break;
 					} else if (i == 2 && prizeNumber == userNumber){
 						prize = maxPrize/2;
-						//console.log('prize is ' + prize + '$');
 						break;
 					} else if (i == 1 && prizeNumber == userNumber){
 						prize = Math.floor((maxPrize/2)/2);
-						//console.log('prize is ' + prize + '$');
 						break;
 					}
+
 					possiblePrize = Math.floor(possiblePrize/2);
 					
 					if (i == 1 && prizeNumber != userNumber && prize == 0){
@@ -49,7 +44,7 @@ var goOn = true;
 
 				if (prizeNumber == userNumber) {
 						goOn = confirm("Congratulation, you guess a number!!! Do you want to continue a game?");
-				}
+				} 
 				totalPrize = totalPrize + prize;
 				if (goOn == true) {
 						max = max * 2;
@@ -57,7 +52,7 @@ var goOn = true;
 						i = 3;
 				} else if (goOn != true && totalPrize != 0){
 						console.log("Thank you for a game. Your prize is: " + totalPrize + "$ Do you want to play again?");
-					}
+				}
 		}
 
 	}else{
